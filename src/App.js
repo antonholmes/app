8d7ef92c44ddd831aac5c54f6cc42613d7e6ee6c
Anton Holmes <holmesanton@icloud.com>
2018-12-06 03:04:45 -0500

    feat(login): added login forms

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCVUEMsM84dpVAfdJsMmrNka-v4NX6doF0',
+      authDomain: 'asset-a6897.firebaseapp.com',
+      databaseURL: 'https://asset-a6897.firebaseio.com',
+      projectId: 'asset-a6897',
+      storageBucket: 'asset-a6897.appspot.com',
+      messagingSenderId: '1055026793565',
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
